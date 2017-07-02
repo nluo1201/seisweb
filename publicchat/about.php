baa2fe708fd8024cd07e0dfdca96393343391346
Herny Luong <nluo1201@gmail.com>
2017-07-01 22:20:41 -0500

    update more files

---
+++ publicchat/about.php
@@ -0,0 +1,24 @@
+ <?php include("./html_inc/header.php"); ?>
+
+<h1> About </h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php  include ("./html_inc/footer.php"); ?>
---
+++ publicchat/contact.php
@@ -0,0 +1,24 @@
+ <?php include("./html_inc/header.php"); ?>
+
+<h1> Contact </h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php  include ("./html_inc/footer.php"); ?>
