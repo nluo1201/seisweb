fd8df2e613131bacacbb4b14016173e4d5b7a815
Luong <luon9752@STTHOMAS.EDU>
2017-07-18 17:47:28 -0500

    update all

--- publicchat/about.php
+++ publicchat/about.php
@@ -8,15 +8,19 @@
 
 <section class="section">
 	<h3> Credits </h3>
-	<p> This is the damn credit section where we have to describe about what the hell we did!</p>
-
+	<p> Below are a list and links to all the 3rd party resources I've used for this project</p>
+	<ul>
+		<li> General tutorials site for knowledge: <a href="https://www.w3schools.com/">w3schools</a>   </li>
+		<li> Canvas tutorials: <a href="http://www.html5canvastutorials.com">html5canvastutorials</a>   </li>
+		<li> Audio and Video resources: <a href="http://www.music.army.mil/music/nationalanthem/">music.army.mil</a>   </li>
+	</ul>
 
 </section>
 
 <section class="section">
 	<h3> Walkthrough </h3>
-	<p> This is the damn Walkthrough section where we have to describe about what the hell we did!</p>
-
+	<p> Below is a description of all requirements and its implementation in this site:</p>
+	<p><br />● Content Map : Used draw.io graphic to intergrate into the page.<br />● Wireframe :&nbsp;Used draw.io graphic to intergrate into the page.<br />● Use HTML5 for markup: obvious thing<br />● Use CSS for layout: obvious thing<br />● The project includes the following 4 top level sections: Home,<br />About, Snippets &amp; Contact<br />● Added a unique favicon created by myself.<br />● CSS Glyphs in footer copyrighted section.<br />● Video element in snippets page.<br />● Audio element of usa national athem in snippets page.<br />● An overview slideshow in snippets page.<br />● Use google's tekkon web font on all headers.<br />● Gradient transition section as canvas element.<br />● Chat and Visualization button as HTML5 form input validation.<br />● JSON for data interchange for message delivery and site visualization updates.<br />● Footer navigation is included.<br />● Sqlite database as of server-side persistent storage for the chat and visualization engines.<br />● Used goolge pie chart as data visualization for site's usage.<br />● Implementation of Shadow DOM accross various usage of javascripts.<br />● Authentication required to access visualization feature.<br />● The About page includes a Credits section that lists and links to<br />the external resources I've used.<br />● Project Walkthrough is this one.<br />● PageSpeed Insights:&nbsp;</p>
 
 </section>
 
