d396c38c57edfe3396bafdd7f30f95120d368dd7
Luong <luon9752@STTHOMAS.EDU>
2017-07-18 16:39:28 -0500

    update again

--- publicchat/html_inc/footer.php
+++ publicchat/html_inc/footer.php
@@ -3,7 +3,7 @@
 
 <footer>
 	<?php  include("./html_inc/footernav.php"); ?>
-	<p class="footer-nav"> Contact information:
+	<p class="footer-nav">
 		<a href="mailto:luon9752@stthomas.edu"> Send me an email! </a>
 	</p>
 	<p class="footer-nav"> Copyright &copy; 2017 Henry Luong </p>
