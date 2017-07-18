90fc9716065f0a7c795a03cefa7910d8ec38b7cc
Luong <luon9752@stthomas.edu>
2017-07-18 18:30:52 -0500

    update walkthrough

--- publicchat/about.php
+++ publicchat/about.php
@@ -20,8 +20,30 @@
 <section class="section">
 	<h3> Walkthrough </h3>
 	<p> Below is a description of all requirements and its implementation in this site:</p>
-	<p><br />● Content Map : Used draw.io graphic to intergrate into the page.<br />● Wireframe :&nbsp;Used draw.io graphic to intergrate into the page.<br />● Use HTML5 for markup: obvious thing<br />● Use CSS for layout: obvious thing<br />● The project includes the following 4 top level sections: Home,<br />About, Snippets &amp; Contact<br />● Added a unique favicon created by myself.<br />● CSS Glyphs in footer copyrighted section.<br />● Video element in snippets page.<br />● Audio element of usa national athem in snippets page.<br />● An overview slideshow in snippets page.<br />● Use google's tekkon web font on all headers.<br />● Gradient transition section as canvas element.<br />● Chat and Visualization button as HTML5 form input validation.<br />● JSON for data interchange for message delivery and site visualization updates.<br />● Footer navigation is included.<br />● Sqlite database as of server-side persistent storage for the chat and visualization engines.<br />● Used goolge pie chart as data visualization for site's usage.<br />● Implementation of Shadow DOM accross various usage of javascripts.<br />● Authentication required to access visualization feature.<br />● The About page includes a Credits section that lists and links to<br />the external resources I've used.<br />● Project Walkthrough is this one.<br />● PageSpeed Insights: all passed: <a href="https://developers.google.com/speed/pagespeed/insights/?url=http%3A%2F%2Flinux2.gps.stthomas.edu%2F~luon9752%2Fpublicchat%2F"> Click to see scores</p>
-
+	<ul>
+	<li><a href="content_map.php">Content Map</a> : Used draw.io graphic to intergrate into the page.</li>
+	<li><a href="wireframe.php">Wireframe </a>:&nbsp;Used draw.io graphic to intergrate into the page.</li>
+	<li>Use HTML5 for markup: obvious thing</li>
+	<li>Use&nbsp;<a href="css/">CSS</a> for layout: I only used bootstrap normalize and style templates no 3rd party tools.</li>
+	<li>The project includes the following <a href="index.php">4 top level sections</a>: Home,&nbsp;About, Snippets &amp; Contact.</li>
+	<li>Added a unique <a href="favicon.ico">favicon</a> created using photoshop.</li>
+	<li>CSS Glyphs in footer copyrighted section.</li>
+	<li>Video element in <a href="snippets.php">snippets </a>page.</li>
+	<li>Audio element of usa national athem in <a href="snippets.php">snippets </a>page.</li>
+	<li>An overview slideshow in <a href="snippets">snippets </a>page.</li>
+	<li>Use google's Tekkon web font on all headers.</li>
+	<li><a href="snippets.php">Gradient transition</a> section as canvas element.</li>
+	<li>Chat and Visualization button as HTML5 <a href="index.php">form input validation</a>.</li>
+	<li>JSON for data interchange for message delivery and site visualization updates.</li>
+	<li>Footer navigation is included.</li>
+	<li><a href="database/">Sqlite database</a> as of server-side persistent storage for the chat and visualization engines.</li>
+	<li>Used goolge pie chart as <a href="snippets.php">data visualization</a> for site's usage.</li>
+	<li>Implementation of Shadow DOM accross various usage of javascripts.</li>
+	<li>Authentication required to access <a href="snippets.php">visualization feature</a>. username: admin, pass: letmein.</li>
+	<li>The <a href="about.php">About</a> page includes a Credits section that lists and links to<br />the external resources I've used.</li>
+	<li>Project <a href="about.php">Walkthrough</a> is this one.</li>
+	<li>PageSpeed Insights: all passed 88/94: <a href="https://developers.google.com/speed/pagespeed/insights/?url=http%3A%2F%2Flinux2.gps.stthomas.edu%2F~luon9752%2Fpublicchat%2F"> Click to see scores</a></li>
+	</ul>
 </section>
 
 
