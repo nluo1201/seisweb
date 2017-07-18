af9949ea08b4b2041fc4c50a21443de3ba09cb82
Luong <luon9752@STTHOMAS.EDU>
2017-07-18 16:53:44 -0500

    update pics

--- publicchat/snippets.php
+++ publicchat/snippets.php
@@ -5,12 +5,10 @@
 <section class="section">
 	<h3> Overview Slide Show </h3>
 	<div class="panel" style="max-width:80%">
-		<img class="mySlides" src="./img/slide01.png" style="width:100%">
-		<img class="mySlides" src="./img/slide02.png" style="width:100%">
-		<img class="mySlides" src="./img/slide03.png" style="width:100%">
-		<img class="mySlides" src="./img/slide04.png" style="width:100%">
-		<img class="mySlides" src="./img/slide05.png" style="width:100%">
-		<img class="mySlides" src="./img/slide06.png" style="width:100%">
+		<img class="mySlides" src="./img/slide01.png" style="height: 100%; width: 100%; object-fit: contain">
+		<img class="mySlides" src="./img/slide02.png" style="height: 100%; width: 100%; object-fit: contain">
+		<img class="mySlides" src="./img/slide03.png" style="height: 100%; width: 100%; object-fit: contain">
+		<img class="mySlides" src="./img/slide04.png" style="height: 100%; width: 100%; object-fit: contain">
 	</div>
 	<script>
 		var myIndex = 0;
