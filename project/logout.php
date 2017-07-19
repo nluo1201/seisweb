0e4a064bc8001391290ee5ee03acffe753dfbefe
Luong <luon9752@STTHOMAS.EDU>
2017-07-18 19:21:14 -0500

    update

--- project/logout.php
+++ project/logout.php
@@ -2,8 +2,8 @@
 <?php
 	if (isset($_SERVER['PHP_AUTH_USER']) &&
 		isset($_SERVER['PHP_AUTH_PW'])){
-			$_SERVER['PHP_AUTH_USER'] = NULL;
-			$_SERVER['PHP_AUTH_PW'] = NULL;	  
+			unset($_SERVER['PHP_AUTH_USER']); 
+			unset($_SERVER['PHP_AUTH_PW'];	  
 		}
 	header("Location: ./index.php"); /* Redirect browser */
 	exit();
