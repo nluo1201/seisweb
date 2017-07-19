116f8dfb47e91a2f00a50efb71cf17bfc9fb5396
Herny Luong <nluo1201@gmail.com>
2017-07-19 11:21:29 -0500

    update

--- project/html_inc/footernav.php
+++ project/html_inc/footernav.php
@@ -25,37 +25,37 @@
 
 
 <table>
-  <tr >
-    <th class="footer-nav"><a href="./index.php">Home</a></th>
-    <th class="footer-nav"><a href="./about.php">About</a></th>
-	<th class="footer-nav"><a href="./snippets.php">Snippets</a></th>
-	<th class="footer-nav"><a href="./contact.php">Contact</a></th>
+  <tr class="footer-tr">
+    <th><a href="./index.php">Home</a></th>
+    <th><a href="./about.php">About</a></th>
+	<th><a href="./snippets.php">Snippets</a></th>
+	<th><a href="./contact.php">Contact</a></th>
   </tr>
-  <tr >
+  <tr class="sub-footer-tr">
     <td><a href="./index.php#chat-butt">Start Chat</a></td>
     <td><a href="./content_map.php">Content Map</a></td>
 	<td><a href="./snippets.php#slideshow">Slideshow</a></td>
     <td></td>
   </tr>
-  <tr >
+  <tr class="sub-footer-tr">
     <td></td>
     <td><a href="./wireframe.php">Wireframe</a></td>
 	<td><a href="./snippets.php#visualization">Visualization</a></td>
     <td></td>
   </tr>
-  <tr>
+  <tr class="sub-footer-tr">
     <td></td>
     <td><a href="./about.php#credits">Credits</a></td>
 	<td><a href="./snippets.php#canvas">Canvas</a></td>
     <td></td>
   </tr>
-  <tr>
+  <tr class="sub-footer-tr">
     <td></td>
     <td><a href="./about.php#walkthrough">Walkthrough</a></td>
 	<td><a href="./snippets.php#video">Video</a></td>
     <td></td>
   </tr>
-  <tr >
+  <tr class="sub-footer-tr">
     <td></td>
     <td></td>
 	<td><a href="./snippets.php#audio">Audio</a></td>
