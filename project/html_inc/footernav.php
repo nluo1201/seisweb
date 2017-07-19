ec2a865e5605dd72124bd0984c1b718988267025
Herny Luong <nluo1201@gmail.com>
2017-07-19 10:52:02 -0500

    update footer

--- project/html_inc/footernav.php
+++ project/html_inc/footernav.php
@@ -4,7 +4,7 @@
 	<a href="./snippets.php">Snippets</a>
 	<a href="./contact.php">Contact</a>
 </nav>
-
+<!--
 <nav class="sub-footer-nav">
 	<a href="./content_map.php">Content Map</a>
 	<a href="./wireframe.php">Wireframe</a>
@@ -16,3 +16,45 @@
 	<a href="./snippets.php#video">Video</a>
 	<a href="./snippets.php#audio">Audio</a>
 </nav>
+
+!-->
+
+
+<table>
+  <tr class="footer-nav">
+    <th><a href="./index.php">Home</a></th>
+    <th><a href="./about.php">About</a></th>
+	<th><a href="./snippets.php">Snippets</a></th>
+	<th><a href="./contact.php">Contact</a></th>
+  </tr>
+  <tr class="sub-footer-nav">
+    <td><a href="./index.php#chat-butt">Start Chat</a></td>
+    <td><a href="./content_map.php">Content Map</a></td>
+	<td><a href="./snippets.php#slideshow">Slideshow</a></td>
+    <td></td>
+  </tr>
+  <tr class="sub-footer-nav">
+    <td></td>
+    <td><a href="./wireframe.php">Wireframe</a></td>
+	<td><a href="./snippets.php#visualization">Visualization</a></td>
+    <td></td>
+  </tr>
+  <tr class="sub-footer-nav">
+    <td></td>
+    <td><a href="./about.php#credits">Credits</a></td>
+	<td><a href="./snippets.php#canvas">Canvas</a></td>
+    <td></td>
+  </tr>
+  <tr class="sub-footer-nav">
+    <td></td>
+    <td><a href="./about.php#walkthrough">Walkthrough</a></td>
+	<td><a href="./snippets.php#video">Video</a></td>
+    <td></td>
+  </tr>
+  <tr class="sub-footer-nav">
+    <td></td>
+    <td></td>
+	<td><a href="./snippets.php#audio">Audio</a></td>
+    <td></td>
+  </tr>
+</table>
